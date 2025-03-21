4969a6389a9a22384d6d8f773d1633a716a93dbd
Francisco Rubial <frubial@fullstacklabs.co>
2025-03-20 21:04:47 -0400

    feat: removing comments

--- webp-converter-server/src/services/imageService.ts
+++ webp-converter-server/src/services/imageService.ts
@@ -1,14 +1,7 @@
 import sharp from 'sharp';
 
 export class ImageService {
-  /**
-   * Convierte una imagen a formato WebP
-   */
   static async convertToWebp(buffer: Buffer): Promise<Buffer> {
     return await sharp(buffer).webp().toBuffer();
   }
-
-  // Podrías añadir otros métodos para diferentes formatos o ajustes
-  // static async convertToJpeg(buffer: Buffer): Promise<Buffer> {...}
-  // static async resizeImage(buffer: Buffer, width: number, height: number): Promise<Buffer> {...}
 }
