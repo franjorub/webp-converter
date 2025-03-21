2b79f0c68645500e7a6bd607d15a3e04cbde83b8
Francisco Rubial <frubial@fullstacklabs.co>
2025-03-20 20:19:28 -0400

    Refactor App container to use Chakra UI Box component
    Replaced the <div> with a <Box> from Chakra UI to provide consistent styling. Added padding, a maximum width, and centered alignment for improved layout and readability. This enhances the UI's responsiveness and overall design.

--- webp-converter-client/src/App.tsx
+++ webp-converter-client/src/App.tsx
@@ -1,12 +1,12 @@
 import FileDropzone from '@/components/features/FileConverter/FileDropzone.tsx';
 import FileList from '@/components/features/FileConverter/FileList.tsx';
 import ConvertButton from '@/components/features/FileConverter/ConvertButton.tsx';
-import { Heading, Stack } from '@chakra-ui/react';
+import { Box, Heading, Stack } from '@chakra-ui/react';
 import { Toaster } from '@/components/ui/toaster.tsx';
 
 export default function App() {
   return (
-    <div>
+    <Box padding={8} maxW="xl" mx="auto">
       <Stack gap={4}>
         <Heading as="h1" size="4xl">
           WebP Converter
@@ -16,6 +16,6 @@ export default function App() {
         <ConvertButton />
       </Stack>
       <Toaster />
-    </div>
+    </Box>
   );
 }
